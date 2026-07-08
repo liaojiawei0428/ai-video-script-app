0b7ab2c8ca4fb256888abf63d56941072836ca90
liaojiawei0428 <liaojiawei0428@gmail.com>
2026-07-08 19:39:42 +0800

    v3.0.107: 提升输入框 maxLength 到 3000 + 实时字数显示 (版本号 bump) (BUG-184)

--- apps/server/ecosystem.config.js
+++ apps/server/ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       env: {
         NODE_ENV: 'production',
         PORT: 6000,
-        APP_VERSION: '3.0.106',
+        APP_VERSION: '3.0.107',
         HTTP_PROXY: '',
         HTTPS_PROXY: '',
         http_proxy: '',
@@ -20,7 +20,7 @@ module.exports = {
       env_production: {
         NODE_ENV: 'production',
         PORT: 6000,
-        APP_VERSION: '3.0.106',
+        APP_VERSION: '3.0.107',
         HTTP_PROXY: '',
         HTTPS_PROXY: '',
         http_proxy: '',
